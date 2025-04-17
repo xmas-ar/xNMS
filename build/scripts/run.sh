6a416bdf647f12fca3fd7055ea9d8cd307da25e6
Christian Mas <79215397+xmas-ar@users.noreply.github.com>
2025-04-17 18:11:43 +0200

    Update run.sh

--- build/scripts/run.sh
+++ build/scripts/run.sh
@@ -36,7 +36,7 @@ function run() {
   export LDAP_ADDR="192.168.56.104"
   export TACACS_ADDR="192.168.56.104"
   export TACACS_PASSWORD="testing123"
-  export SLACK_TOKEN="xoxb-3363714037463-3375475639189-Z3p5pezp3X4N3QKLHkxX3pzu"
+  export SLACK_TOKEN="xoxb-XXX"
   export FLASK_APP="app.py"
   export FLASK_DEBUG=1
   if [ "$database" = "mysql" ]; then
